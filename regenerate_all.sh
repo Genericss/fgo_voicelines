a77fe83363d6f41bed6188a1dae09ec2e1e5c647
Justin Ng <justng24@gmail.com>
2024-07-07 20:57:54 -0400

    added python code to add the grouped voicelines, may need to clean up later

--- regenerate_all.sh
+++ regenerate_all.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+old_dir=$(pwd)
 cd ./used_code/
 ./convert_json_lore_to_csv.sh
 ./get_only_voicelines_from_csv.sh
 ./convert_voiceline_csv_to_dot.sh
-
+./convert_all_voiceline_csv_to_dot.sh
+cd $old_dir
 
---
+++ used_code/convert_all_voiceline_csv_to_dot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 generate_gephi_dot_all_voices.py ../generated_data/only_servant_voicelines.csv ../generated_data/all_servant_voices.gv
