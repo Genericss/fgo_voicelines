666d919750f305a482876ca2c8432b7ca5047b35
Justin Ng <justng24@gmail.com>
2024-07-13 22:00:34 -0400

    Added some decent output images

--- launch_gephi.sh
+++ launch_gephi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 javac -cp "./downloaded_libraries/gephi-toolkit-0.10.0-all.jar" ./interactGephi.java
-#java -cp ".:./downloaded_libraries/gephi-toolkit-0.10.0-all.jar:./generated_data/servant_voices.gv" ./Main.java
+java -cp ".:./downloaded_libraries/gephi-toolkit-0.10.0-all.jar:./generated_data/servant_voices.gv" ./Main.java
 
-java --illegal-access=permit -cp ".:./downloaded_libraries/gephi-toolkit-0.10.0-all.jar:./generated_data/servant_voices.gv" ./Main.java
+#java --illegal-access=permit -cp ".:./downloaded_libraries/gephi-toolkit-0.10.0-all.jar:./generated_data/servant_voices.gv" ./Main.java
 
