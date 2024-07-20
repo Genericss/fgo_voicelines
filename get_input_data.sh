d3953a3d403f1f97a4782797314a84e6746e11b6
Justin Ng <justng24@gmail.com>
2024-07-19 22:17:16 -0400

    updated input data

---
+++ get_input_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+old_dir=$(pwd)
+cd ./input_data/
+
+if [ -f ./nice_servant_lore.json ]; then
+	last_modified=$(date -r ./nice_servant_lore.json "+%Y-%m-%d")
+	mkdir -p $last_modified
+	mv ./nice_servant_lore.json ./${last_modified}
+fi
+
+curl https://api.atlasacademy.io/export/NA/nice_servant_lore.json > ./nice_servant_lore.json
+
+cd ${old_dir}
